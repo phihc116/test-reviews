d7c1f20b42b467e93a31262b760159c0322cafce
Hoàng Phi <phi.hoang@herond.org>
2025-04-11 15:09:34 +0700

    feat: add reviewer

---
+++ CodeSmell.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace SmellyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new Database();
+            db.Connect();
+            db.Query("SELECT * FROM Users WHERE isAdmin = 1");
+
+            var user = new User();
+            user.name = "admin";
+            user.age = -5;
+            user.password = "123";
+
+            Console.WriteLine("Done");
+        }
+    }
+
+    class Database
+    {
+        public void Connect()
+        {
+            Console.WriteLine("Connected to DB.");
+        }
+
+        public void Query(string q)
+        {
+            Console.WriteLine("Running query: " + q);
+        }
+    }
+
+    class User
+    {
+        public string name;
+        public int age;
+        public string password;
+    }
+}
